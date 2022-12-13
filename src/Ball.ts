35a17fd7e196d56d5a1094fc2410a932857d627c
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-12-13 09:37:09 +0200

    add direction to the ball based on collision

--- src/Ball.ts
+++ src/Ball.ts
@@ -1,20 +1,73 @@
+import { HEIGHT, WIDTH } from "./app";
+import { Brick } from "./Brick";
+import { Paddle } from "./Paddle";
 
 export class Ball {
-    private ctx: CanvasRenderingContext2D
-    constructor(
-      renderingCtx: CanvasRenderingContext2D,
-      public x: number = 50, 
-      public y: number = 150,
-      public radius: number = 10
+  private ctx: CanvasRenderingContext2D;
+  constructor(
+    renderingCtx: CanvasRenderingContext2D,
+    public x: number,
+    public y: number,
+    public vel: { x: number; y: number },
+    public radius: number = 10
+  ) {
+    this.ctx = renderingCtx;
+  }
+
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    this.ctx.fillStyle = "#0095DD";
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
+  move() {
+    this.x += this.vel.x;
+    this.y += this.vel.y;
+  }
+
+  checkCollision(paddle: Paddle, bricks: Brick[]) {
+    this.checkForCollisionWithPerimeter();
+    this.checkForCollisionWithPaddle(paddle);
+    this.checkForCollisionWithBricks(bricks);
+  }
+
+  checkForCollisionWithPerimeter() {
+    if (
+      this.x + this.vel.x > WIDTH - this.radius ||
+      this.x + this.vel.x < this.radius
     ) {
-        this.ctx = renderingCtx;
+      this.vel.x = -this.vel.x;
     }
-  
-    draw() {
-      this.ctx.beginPath();
-      this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-      this.ctx.fillStyle = "#0095DD";
-      this.ctx.fill();
-      this.ctx.closePath();
+
+    if (this.y + this.vel.y < this.radius) {
+      this.vel.y = -this.vel.y;
     }
   }
+
+  checkForCollisionWithPaddle(paddle: Paddle) {
+    if (this.y + this.vel.y > HEIGHT - this.radius - paddle.height - 10) {
+      if (this.x > paddle.x && this.x < paddle.x + paddle.width) {
+        this.vel.y = -this.vel.y;
+        let centerOfPaddleX = paddle.x + paddle.width / 2;
+        let ballDistFromPaddleCenterX = this.x - centerOfPaddleX;
+        this.vel.x = ballDistFromPaddleCenterX * 0.05;
+      }
+    }
+  }
+
+  checkForCollisionWithBricks(bricks: Brick[]) {
+    for (const brick of bricks) {
+      if (
+        this.x > brick.x &&
+        this.x < brick.x + brick.width &&
+        this.y + this.radius > brick.y &&
+        this.y - this.radius < brick.y + brick.height
+      ) {
+        this.vel.y = -this.vel.y;
+        brick.isHitted = true;
+      }
+    }
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,13 @@ export const WIDTH = canvas.width;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
-let dx = 1.5;
-let dy = 2;
+const speed = 2;
+const vel = {
+  x: speed,
+  y: speed * 1.5,
+};
 
-const ball = new Ball(ctx, x, y);
+const ball = new Ball(ctx, x, y, vel);
 const paddle = new Paddle(ctx, (canvas.width - 150) / 2);
 let bricks = generateBricks();
 
@@ -36,8 +39,7 @@ function render() {
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
   ball.draw();
   checkIsBallColliding(ball);
-  ball.x += dx;
-  ball.y += dy;
+  ball.move();
 
   paddle.draw();
   drawLives();
@@ -50,7 +52,7 @@ function render() {
     }
   }
 
-  detectCollision(ball);
+  checkCollisionWithBricks(ball);
 
   if (alive) {
     requestAnimationFrame(render);
@@ -58,30 +60,33 @@ function render() {
 }
 
 function checkIsBallColliding(ball: Ball) {
-  if (ball.x + dx > WIDTH - ball.radius || ball.x + dx < ball.radius) {
-    dx = -dx;
+  if (ball.x + vel.x > WIDTH - ball.radius || ball.x + vel.x < ball.radius) {
+    vel.x = -vel.x;
   }
 
-  if (ball.y + dy < ball.radius) {
-    dy = -dy;
-  } else if (ball.y + dy > HEIGHT - ball.radius - paddle.height - 10) {
+  if (ball.y + vel.y < ball.radius) {
+    vel.y = -vel.y;
+  } else if (ball.y + vel.y > HEIGHT - ball.radius - paddle.height - 10) {
     if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-      dy = -dy;
+      vel.y = -vel.y;
+      let centerOfPaddleX = paddle.x + paddle.width / 2;
+      let ballDistFromPaddleCenterX = ball.x - centerOfPaddleX;
+      vel.x = ballDistFromPaddleCenterX * 0.05;
     } else {
-      if (ball.y + dy > HEIGHT - ball.radius) {
+      if (ball.y + vel.y > HEIGHT - ball.radius) {
         lives--;
         if (!lives) {
           ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.font = '24px Arial';
-          ctx.textAlign = 'center';
-          ctx.textBaseline = 'middle';
-          ctx.fillText('Game Over', canvas.width / 2, canvas.height / 2);
+          ctx.font = "24px Arial";
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2);
           alive = false;
         } else {
           ball.x = canvas.width / 2;
           ball.y = canvas.height - 30;
-          dx = 2;
-          dy = -2;
+          vel.x = 2;
+          vel.y = -2;
           paddle.x = (canvas.width - paddle.width) / 2;
         }
       }
@@ -110,7 +115,7 @@ function generateBricks() {
   return bricks;
 }
 
-function detectCollision(ball: Ball) {
+function checkCollisionWithBricks(ball: Ball) {
   for (const brick of bricks) {
     if (
       ball.x > brick.x &&
@@ -118,29 +123,29 @@ function detectCollision(ball: Ball) {
       ball.y + ball.radius > brick.y &&
       ball.y - ball.radius < brick.y + brick.height
     ) {
-      dy = -dy;
+      vel.y = -vel.y;
       brick.isHitted = true;
       score++;
       if (score === brickColumnCount * brickRowCount) {
         alive = false;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.font = '24px Arial';
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        ctx.fillText('You WIN', canvas.width / 2, canvas.height / 2);
+        ctx.font = "24px Arial";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText("You WIN", canvas.width / 2, canvas.height / 2);
       }
     }
   }
 }
 
 function drawScore() {
-  ctx.font = '16px Arial';
-  ctx.fillStyle = '#0095DD';
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
   ctx.fillText(`Score: ${score}`, 8, 20);
 }
 
 function drawLives() {
-  ctx.font = '16px Arial';
-  ctx.fillStyle = '#0095DD';
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
   ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
 }
